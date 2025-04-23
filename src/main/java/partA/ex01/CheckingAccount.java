444790ef981b69e41d64e0559c651bc2df6367a3
Lexus Adjei <lexusadjei2016@gmail.com>
2025-04-23 16:59:02 -0400

    Push for 4/23
    yeah I might redo this. I don't know

--- src/main/java/partA/ex01/CheckingAccount.java
+++ src/main/java/partA/ex01/CheckingAccount.java
@@ -1,6 +1,24 @@
 package partA.ex01;
 
 public class CheckingAccount {
+    public double curBalance;
+    public String name;
 
-
+public void withdrawal(int amount){
+    curBalance -= amount;
 }
+
+    public void displayCustomer(){
+        System.out.println("Customer: "+name);
+        System.out.println("Balance: " + curBalance);
+    }
+
+    public static void main(String[] args) {
+        CheckingAccount account = new CheckingAccount();
+        account.name = "Johnny";
+        account.curBalance = 600.00;
+        account.withdrawal(50);
+        account.displayCustomer();
+    }
+}
+
